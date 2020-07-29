4503e0181bf4f991d12d5099da9e1cb10faae00f
B. Lipnik <barbara7777>
2020-07-29 21:07:03 +0200

    odstranila testne funkcije

--- umetnine/account/forms.py
+++ umetnine/account/forms.py
@@ -58,22 +58,12 @@ class UserForm(forms.ModelForm):
         else:
             raise forms.ValidationError("This is not a valid year.")
 
-    def clean_name(self):
-        data = self.cleaned_data.get('name')
-        if not 'B' in data:
-            print("napaka v imenu - manjka B")
-            raise forms.ValidationError('to ni pravo ime.')
-        return data
-
     def clean(self, *args, **kwargs):
         cleaned_data = super().clean()
-        print(cleaned_data)
         p1 = cleaned_data.get('password1')
         p2 = cleaned_data.get('password2')
         if (not p1 == p2) and p1 and p2:
-            print("----NISO PRAVA GESLA")
             raise forms.ValidationError('Passwords do not match.')
-
 
 
 class UserLoginForm(forms.ModelForm):
