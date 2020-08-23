a7ca1e1ef55ba86661b2fce96d3c8c10c0434fc6
Tomaž Derča <tomaz.derca@gmail.com>
2020-08-23 20:22:52 +0200

    Urejanje prve strani. Pagination.

--- umetnine/artists/urls.py
+++ umetnine/artists/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from .views import PostListView
 
 app_name = 'artists'
 
 urlpatterns = [
-    path('uporabniki/', views.uporabniki, name='artists-uporabnik')
+    path('uporabniki/', PostListView.as_view(), name='artists-uporabnik')
 
 ]
--- umetnine/artists/views.py
+++ umetnine/artists/views.py
@@ -1,7 +1,8 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Arts
 from django.contrib.auth.models import User
-
+from django.views.generic import ListView
 #
 # def home(request):
 #     return HttpResponse('<h1>Domaca stran</h1>')
@@ -16,3 +17,17 @@ def uporabniki(request):
         'artworks': Arts.objects.all()
     }
     return render(request, 'artists/uporabniki.html', context)
+
+class PostListView(ListView):
+    model = User
+    template_name = 'artists/uporabniki.html'
+    context_object_name = 'users'
+    paginate_by = 10
+
+# def uporabniki2(request):
+#     users = User.objects.all()
+#     artworks = Arts.objects.all()
+#     paginator = Paginator(users, 10)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#     return render(request, 'artists/uporabniki.html', {'page_obj': page_obj, 'users': users, 'artwor'})
