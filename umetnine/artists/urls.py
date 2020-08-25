37b76e2f80aef647e599bdb3ba1a74c59b709664
barbara7777 <badobe.lipnik@gmail.com>
2020-08-25 13:21:02 +0200

    dodala like gumb na slike

--- umetnine/artists/urls.py
+++ umetnine/artists/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import PostListView, dynamic_artwork_lookup_view
+from .views import PostListView, dynamic_artwork_lookup_view, art_like
 
 app_name = 'artists'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='artists-uporabnik'),
-    path('artworks/<int:id>/', dynamic_artwork_lookup_view, name='artwork')
+    path('artworks/<int:id>/', dynamic_artwork_lookup_view, name='artwork'),
+    path('like/<int:id>/', art_like, name='art_like'),
 
 
 ]
--- umetnine/artists/views.py
+++ umetnine/artists/views.py
@@ -1,14 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Arts
 from django.contrib.auth.models import User
 from django.views.generic import ListView
-#
-# def home(request):
-#     return HttpResponse('<h1>Domaca stran</h1>')
-#
-#
-# def about(request):
-#     return HttpResponse('<h1>Artists About</h1>')
+
 
 def uporabniki(request):
     context = {
@@ -17,6 +11,20 @@ def uporabniki(request):
     }
     return render(request, 'artists/uporabniki.html', context)
 
+
+def art_like(request, id):
+    art_to_like = get_object_or_404(Arts, pk=id)
+    try:
+        art_to_like.likes += 1
+        art_to_like.save()
+    finally:
+        context = {
+            'art': art_to_like,
+            'id': id,
+        }
+        return render(request, 'artists/artwork.html', context)
+
+
 class PostListView(ListView):
     model = Arts
     template_name = 'artists/uporabniki.html'
@@ -24,12 +32,15 @@ class PostListView(ListView):
     ordering = ['-timestamp']
     paginate_by = 9
 
+
 def dynamic_artwork_lookup_view(request, id):
     art = Arts.objects.get(id=id)
     context = {
-        'art': art
+        'art': art,
+        'id': id,
     }
     return render(request, 'artists/artwork.html', context)
+
 
 def dynamic_user_lookup_view(request, id):
     user = User.objects.get(id=id)
