68b358aefea402c353ae7fc75111299f315f116f
Tomaž Derča <tomaz.derca@gmail.com>
2020-08-26 16:42:42 +0200

    Estetske olepšave homepage v2.

--- umetnine/artists/static/artists/jq.js
+++ umetnine/artists/static/artists/jq.js
@@ -24,3 +24,16 @@ $(this).removeClass("active");
 $(this).addClass("active");
 
 });
+
+$(function() {
+var selectedClass = "";
+$(".filter").click(function(){
+selectedClass = $(this).attr("data-rel");
+$("#gallery").fadeTo(100, 0.1);
+$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+setTimeout(function() {
+$("."+selectedClass).fadeIn().addClass('animation');
+$("#gallery").fadeTo(300, 1);
+}, 300);
+});
+});
