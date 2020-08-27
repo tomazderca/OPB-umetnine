5bc8fd76c4df9574fa7b3de85f33b715a9c6c408
Tomaž Derča <tomaz.derca@gmail.com>
2020-08-27 12:06:38 +0200

    url popravljanje.

--- umetnine/artists/urls.py
+++ umetnine/artists/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='artists-uporabnik'),
     path('<int:user_id>/<int:artwork_id>/', views.dynamic_artwork_lookup_view, name='user-artwork'),
     path('like/<int:artwork_id>/', views.artwork_like_api_toggle, name="api-like-toggle"),
+    path('<int:user_id>/', views.dynamic_user_lookup_view, name='user-user')
 ]
